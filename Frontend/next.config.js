96c09ffcac417968d5d0cce308d33054dae193cf
Yuvraj Singh <72330657+Yuvraj-ai@users.noreply.github.com>
2025-06-04 03:19:46 +0530

    Update next.config.js

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
+    BACKEND_URL: process.env.BACKEND_URL || 'genaibe-production.up.railway.app',
   },
 };
